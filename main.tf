f1a07d7784facba66e6d209e0e8a75316a0337de
shrinivas <shinivassb16@gmail.com>
2024-01-20 23:24:35 +0530

    updated

--- main.tf
+++ main.tf
@@ -3,16 +3,18 @@ resource "aws_security_group" "main" {
   description = local.name
   vpc_id      = var.vpc_id
 
-  dynamic "ingress" {
-    for_each = var.ingress_ports
-    content {
-    description      = "APP"
-    from_port        = ingress.value["port"]
-    to_port          = ingress.value["port"]
+  ingress {
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = var.sg_cidrs
-      
-    }
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_cidrs
   }
   
 
@@ -36,49 +38,40 @@ resource "aws_lb" "main" {
   tags = merge(var.tags, {Name =  local.name})
 }
 
-# resource "aws_lb_listener" "main" {
-#   count             = var.enable_https ? 0 : 1
-#   load_balancer_arn = aws_lb.main.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
   
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = var.target_group_arn
-#   }
-# }
+  default_action {
+    type = "fixed-response"
 
-# resource "aws_lb_listener" "https" {
-#   count             = var.enable_https ? 1 : 0
-#   load_balancer_arn = aws_lb.main.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = var.certificate_arn
-  
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = var.target_group_arn
-#   }
-# }
-# resource "aws_lb_listener" "http" {
-#   count             = var.enable_https ? 1 : 0
-#   load_balancer_arn = aws_lb.main.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "ERROR"
+      status_code  = "500"
+    }
+  }
+}
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
   
   
-#   default_action {
-#     type = "redirect"
+  default_action {
+    type = "redirect"
     
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
  
-# }
+}
 
 # resource "aws_route53_record" "main" {
 #   name    = var.dns_name == null ? "${var.component}-${var.env}" : var.dns_name
--- vars.tf
+++ vars.tf
@@ -6,9 +6,7 @@ variable "sg_cidrs" {}
 variable "tags" {}
 variable "subnets" {}
 variable "route53_zone_id" {}
-variable "enable_https" {}
 variable "certificate_arn" {}
-variable "ingress_ports" {}
 variable "dns_name" {
   default = null
 }
